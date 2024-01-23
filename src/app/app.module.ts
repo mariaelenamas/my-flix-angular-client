c64e4457e320994255355eaefc0e74c4fbe8cb0b
mariaelenamas <mariaelenamassarelli@gmail.com>
2024-01-24 00:33:06 +0100

    Routes updated + other minor adjustments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
+import { RouterModule } from '@angular/router';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-const appRoutes: Routes = [
-  { path: 'welcome', component: WelcomePageComponent },
-  { path: 'movies', component: MovieCardComponent },
-  { path: '/', redirectTo: 'welcome', pathMatch: 'prefix' },
-];
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { routes } from './app.routes'
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserRegistrationFormComponent
+    MovieCardComponent,
+    UserRegistrationFormComponent,
+    WelcomePageComponent,
   ],
   imports: [
+    BrowserModule,
     CommonModule,
     HttpClientModule,
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
     MatDialogModule,
@@ -47,11 +42,11 @@ const appRoutes: Routes = [
     MatIconModule,
     MatFormFieldModule,
     MatSnackBarModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
   ],
   exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-
 export class AppModule { }
