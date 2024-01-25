2baf17b021b99c2b1c6124b8a8cc2d314872810d
mariaelenamas <mariaelenamassarelli@gmail.com>
2024-01-26 00:28:37 +0100

    Login view updated

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,16 +1,55 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatDialogRef } from '@angular/material/dialog';
+
+// This import brings in the API calls we created in 6.2
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-login-form',
-  standalone: true,
   templateUrl: './user-login-form.component.html',
   styleUrls: ['./user-login-form.component.scss']
 })
 export class UserLoginFormComponent {
-  constructor(private router: Router) { }
+  @Input() loginData = { Username: '', Password: '' };
 
-  navigateToMovies(): void {
-    this.router.navigate(['movies']);
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+  /**
+   * This method will send the form inputs to the backend
+   * @param void
+   * @returns user object
+   * @memberof UserLoginFormComponent
+   * @see FetchApiDataService.userLogin()
+   * @example loginUser()
+   */
+  // This is the function responsible for sending the form inputs to the backend
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.loginData).subscribe((result) => {
+      // Logic for a successful user login goes here! (To be implemented)
+      localStorage.setItem('user', JSON.stringify(result.user));
+      localStorage.setItem('token', result.token);
+      this.dialogRef.close(); // This will close the modal on success!
+      this.router.navigate(['movies']);
+      this.snackBar.open('Logged in', 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
   }
 }
+
+
